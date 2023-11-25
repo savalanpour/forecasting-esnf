3f26b7602762db83a57dce5bb05eaf78a4ce9552
savalanpour <m.savalanpour@gmail.com>
2023-11-25 01:07:48 -0700

    Create other time filters

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,7 +3,8 @@ import Highcharts from 'highcharts/highstock';
 import HighchartsReact from 'highcharts-react-official';
 import s from "./Chart.module.css";
 import dayjs from "dayjs";
-import {Button} from "antd";
+import {Button, Radio} from "antd";
+// import axios from 'axios';
 
 const Chart = () => {
   const chartRef = useRef();
@@ -12,21 +13,59 @@ const Chart = () => {
   const [y, setY] = useState(null)
   const [last, setLast] = useState(null)
   const [loading, setLoading] = useState(false)
+  const [type, setType] = useState('d1');
   
   useEffect(() => {
+    //"BINANCE"
+    // BTC
+    // let config = {
+    //   method: 'get',
+    //   maxBodyLength: Infinity,
+    //   url: 'https://rest.coinapi.io/v1/ohlcv/BINANCE_SPOT_BTC_USDT/history?period_id=1HRS&time_start=2021-03-01T00:00:00&limit=1000',
+    //   headers: {
+    //     'Accept': 'text/plain',
+    //     // 'X-CoinAPI-Key': 'E9C06A59-E16F-4B47-8B99-DAADD9A07685'
+    //     'X-CoinAPI-Key': 'A24A2275-D9C1-4C69-88EF-F8AEA1B52808'
+    //   }
+    // };
+    //
+    // let x = [], y = []
+    // axios(config)
+    // .then((response) => {
+    //   console.log(response.data);
+    //   response?.data.forEach(item => {
+    //     x.push(Math.round(item.price_close))
+    //     y.push(item.time_close)
+    //   })
+    //   setX(x)
+    //   setY(y)
+    //   // setLast(response?.data?.length)
+    // })
+    // .catch((error) => {
+    //   console.log(error);
+    // });
+    //
     const requestOptions = {
       method: 'GET',
       redirect: 'follow'
     };
-  
-    fetch("https://api.coincap.io/v2/assets/bitcoin/history?interval=m30", requestOptions)
+    let url;
+    const today = new Date();
+    const Date180 = new Date(new Date().setDate(today.getDate() - 180));
+    const Date60 = new Date(new Date().setDate(today.getDate() - 60));
+    const Date30 = new Date(new Date().setDate(today.getDate() - 29));
+    if(type === "d1") url = `https://api.coincap.io/v2/assets/bitcoin/history?interval=d1&start=1411470720000&end=${+today}`;
+    if(type === "h6") url = `https://api.coincap.io/v2/assets/bitcoin/history?interval=h6&start=${+Date180}&end=${+today}`;
+    if(type === "h2") url = `https://api.coincap.io/v2/assets/bitcoin/history?interval=h2&start=${+Date60}&end=${+today}`;
+    if(type === "h1") url = `https://api.coincap.io/v2/assets/bitcoin/history?interval=h1&start=${+Date30}&end=${+today}`;
+    fetch(url, requestOptions)
     .then(response => response.json())
     .then(result => {
       setData(result.data)
       let x = [], y = []
       result?.data.forEach(item => {
         x.push(+item.priceUsd)
-        y.push(dayjs(item.date).format('DD MMM'))
+        y.push(dayjs(item.date).format('YYYY DD MMM'))
       })
       setX(x)
       setY(y)
@@ -34,7 +73,8 @@ const Chart = () => {
       setLast(result?.data.length)
     })
     .catch(error => console.log('error', error));
-  }, [])
+  }, [type])
+  
   const chartOptions = {
     chart: {
       type: 'spline',
@@ -106,6 +146,16 @@ const Chart = () => {
   
   return (
     <div className="content-box">
+      <Radio.Group
+        value={type}
+        style={{display: "flex", width: "70%", margin: "0 auto 20px"}}
+        onChange={(e) => setType(e.target.value)}
+      >
+        <Radio.Button style={{flex: 1, textAlign: "center"}} value="h1">H1</Radio.Button>
+        <Radio.Button style={{flex: 1, textAlign: "center"}} value="h2">H2</Radio.Button>
+        <Radio.Button style={{flex: 1, textAlign: "center"}} value="h6">H6</Radio.Button>
+        <Radio.Button style={{flex: 1, textAlign: "center"}} value="d1">Daily</Radio.Button>
+      </Radio.Group>
       <div style={{minHeight: "600px"}}>
         <div className={s.chart}>
           {data ? <HighchartsReact
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,21 @@ import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import dynamic from 'next/dynamic'
 import Menu from "@/components/Menu/Menu";
-import {Button} from "antd";
+import {Button, Radio} from "antd";
 const Chart = dynamic(() => import('../components/Chart/Chart'), {
   ssr: false,
 })
 
 export default function Home() {
   return (
-    <main className="content">
-      <div className="border-r-gray-200 border-r">
-        <div className="logo">ENSF 619</div>
-        <Menu />
-      </div>
-      <Chart />
-    </main>
+    <div>
+      <main className="content">
+        <div className="border-r-gray-200 border-r">
+          <div className="logo">ENSF 619</div>
+          <Menu />
+        </div>
+        <Chart />
+      </main>
+    </div>
   )
 }
